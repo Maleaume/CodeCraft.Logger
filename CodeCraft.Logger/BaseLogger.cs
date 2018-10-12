f330624e1bff0482db0e8786ebf417b8e1ff88c3
Maleaume BONNIN <maleaume@gmail.com>
2018-10-13 01:06:48 +0200

    Renaming

--- CodeCraft.Logger/BaseLogger.cs
+++ CodeCraft.Logger/BaseLogger.cs
@@ -10,20 +10,20 @@ namespace CodeCraft.Logger
         private readonly T logProducerConsumer = new T();
 
         #region Lazy Objects
-        private readonly Lazy<ILevelLogFormatter> traceLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Trace), true);
-        private readonly Lazy<ILevelLogFormatter> infoLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Info), true);
-        private readonly Lazy<ILevelLogFormatter> debugLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Debug), true);
-        private readonly Lazy<ILevelLogFormatter> warningLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Warning), true);
-        private readonly Lazy<ILevelLogFormatter> errorLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Error), true);
-        private readonly Lazy<ILevelLogFormatter> criticalLogger = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Critical), true);
+        private readonly Lazy<ILevelLogFormatter> traceLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Trace), true);
+        private readonly Lazy<ILevelLogFormatter> infoLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Info), true);
+        private readonly Lazy<ILevelLogFormatter> debugLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Debug), true);
+        private readonly Lazy<ILevelLogFormatter> warningLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Warning), true);
+        private readonly Lazy<ILevelLogFormatter> errorLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Error), true);
+        private readonly Lazy<ILevelLogFormatter> criticalLogFormatter = new Lazy<ILevelLogFormatter>(() => InstanciateLevelLoger(ElogLevel.Critical), true);
         #endregion
         #region Accessors
-        protected ILevelLogFormatter TraceLogger => traceLogger.Value;
-        protected ILevelLogFormatter InfoLogger => infoLogger.Value;
-        protected ILevelLogFormatter DebugLogger => debugLogger.Value;
-        protected ILevelLogFormatter WarningLogger => warningLogger.Value;
-        protected ILevelLogFormatter ErrorLogger => errorLogger.Value;
-        protected ILevelLogFormatter CriticalLogger => criticalLogger.Value;
+        protected ILevelLogFormatter TraceLogFormatter => traceLogFormatter.Value;
+        protected ILevelLogFormatter InfoLogFormatter => infoLogFormatter.Value;
+        protected ILevelLogFormatter DebugLogFormatter => debugLogFormatter.Value;
+        protected ILevelLogFormatter WarningLogFormatter => warningLogFormatter.Value;
+        protected ILevelLogFormatter ErrorLogFormatter => errorLogFormatter.Value;
+        protected ILevelLogFormatter CriticalLogFormatter => criticalLogFormatter.Value;
         #endregion
 
         private static ILevelLogFormatter InstanciateLevelLoger(ElogLevel logLevel)
@@ -34,12 +34,12 @@ namespace CodeCraft.Logger
 
         protected abstract void WriteLog(string log);
 
-        public void Trace(string log) => EnqueueLog(log, TraceLogger);
-        public void Info(string log) => EnqueueLog(log, InfoLogger);
-        public void Debug(string log) => EnqueueLog(log, DebugLogger);
-        public void Warn(string log) => EnqueueLog(log, WarningLogger);
-        public void Error(string log) => EnqueueLog(log, ErrorLogger);
-        public void Critical(string log) => EnqueueLog(log, CriticalLogger);
+        public void Trace(string log) => EnqueueLog(log, TraceLogFormatter);
+        public void Info(string log) => EnqueueLog(log, InfoLogFormatter);
+        public void Debug(string log) => EnqueueLog(log, DebugLogFormatter);
+        public void Warn(string log) => EnqueueLog(log, WarningLogFormatter);
+        public void Error(string log) => EnqueueLog(log, ErrorLogFormatter);
+        public void Critical(string log) => EnqueueLog(log, CriticalLogFormatter);
 
         public void Dispose() => logProducerConsumer.Dispose();
     }
